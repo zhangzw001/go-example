10cda8c170288672f2dc5595efbf93da4fe60f21
zhangzw <zhangzw@boqii.com>
2020-07-01 11:39:22 +0800

    2020-07-01 11:39:22

--- go圣经/05/5.2/5.2outline.go
+++ go圣经/05/5.2/5.2outline.go
@@ -7,18 +7,31 @@ import (
 	"net/http"
 )
 
-func outline(stack []string, n *html.Node) {
+func outline(stack []string, n *html.Node) []string {
 	if n.Type == html.ElementNode {
 		stack = append(stack , n.Data) 	//push tag
 		fmt.Println(stack )
 	}
 	for c := n.FirstChild; c != nil ; c = c.NextSibling {
-		outline(stack, c )
+		stack = outline(stack, c )
 	}
+	return stack
 }
 
+func outlinecount( n *html.Node) map[string]int {
+	outlinemap := make(map[string]int )
+	fmt.Println(outlinemap)
+	if n.Type == html.ElementNode {
+		outlinemap[n.Data]++
+	}
+	for c := n.FirstChild; c != nil ; c = c.NextSibling {
+		outlinemap = outlinecount(c)
+	}
+	return outlinemap
+}
 func main() {
 	//resp, err := http.Get(os.Args[1])
+	//resp, err := http.Get("http://localhost/5.2findlinks2_1.html")
 	resp, err := http.Get("https://www.baidu.com")
 	if err != nil { log.Fatal(err)}
 	if resp.StatusCode != http.StatusOK {
@@ -32,7 +45,10 @@ func main() {
 		log.Fatal(err)
 	}
 	//outline(nil, doc)
-	sourceStack := []string{"1"}
-	outline(sourceStack,doc)
-	fmt.Println(sourceStack)
+	//sourceStack := []string{}
+	//sourceStack = outline(sourceStack,doc)
+	//fmt.Println(sourceStack)
+	outlineMap := make(map[string]int)
+	outlineMap = outlinecount(doc)
+	fmt.Println(outlineMap)
 }
