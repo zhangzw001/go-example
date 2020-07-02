ce31840a6153a06ffc915d28fdef0295716e35a6
zhangzw <zhangzw@boqii.com>
2020-07-02 14:39:22 +0800

    2020-07-02 14:39:22

--- go圣经/05/5.5/5.5outline2.go
+++ go圣经/05/5.5/5.5outline2.go
@@ -38,17 +38,43 @@ func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 }
 
 var depth int
+//练习 5.7： 完善startElement和endElement函数，使其成为通用的HTML输出器。要求：输出注释结点，文本结点以及每个元素的属性（< a href='...'>）。使用简略格式输出没有孩子结点的元素（即用<img/>代替<img>
+//</img>）。编写测试，验证程序输出的格式正确。（详见11章）
+//优化了script标签问题
 func startElement(n *html.Node) {
 	if n.Type == html.ElementNode {
 		attr := ""
-		for _, a := range 
-		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+		for _, a := range n.Attr {
+			attr = fmt.Sprintf(" %s=\"%s\" ",a.Key,a.Val)
+			//attr += " " + a.Key + "=" + "\"" + a.Val + "\" "
+			fmt.Println(a.Key, a.Val, n.Data, attr)
+
+		}
+		fmt.Printf("%*s<%s%s", depth*2, "", n.Data,attr)
 		depth++
+	}
+	if n.Type == html.ElementNode && n.FirstChild == nil && n.Data != "script" {
+		fmt.Printf("/>\n")
+	} else if n.Type == html.ElementNode {
+		fmt.Printf(">\n")
+	}
+
+	if n.Type == html.TextNode {
+		fmt.Printf("%*s %s\n", depth*2,"",n.Data)
 	}
 }
 func endElement(n *html.Node) {
+	if n.Type == html.ElementNode && n.FirstChild == nil && n.Data != "script" {
+		depth--
+		if n.FirstChild != nil {
+			
+		}
+		fmt.Printf("\n")
+		return
+	}
 	if n.Type == html.ElementNode {
 		depth--
+
 		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
 	}
 }
