818bfcf14eed50797f6c76ceccba59976c50fe97
zhangzw <zhangzw@boqii.com>
2020-07-03 09:39:28 +0800

    2020-07-03 09:39:28

--- go圣经/05/5.6/5.6links.go
+++ go圣经/05/5.6/5.6links.go
@@ -8,9 +8,11 @@ import (
 )
 
 func main() {
-	a, err := Extract("http://localhost/5.2findlinks2_1.html")
-	if err != nil { log.Fatal(err)}
-	fmt.Println(a)
+	//a, err := Extract("http://localhost/5.2findlinks2_1.html")
+	//if err != nil { log.Fatal(err)}
+	//fmt.Println(a)
+	breadthFirst(crawl, []string{"http://localhost/5.2findlinks2_1.html"})
+
 }
 
 
@@ -63,4 +65,30 @@ func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
 	if post != nil {
 		post(n)
 	}
+}
+
+// breadthFirst calls f for each item in the worklist.
+// Any items returned by f are added to the worklist.
+// f is called at most once for each item.
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
 }
