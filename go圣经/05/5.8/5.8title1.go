079e28e0d87e8ccdac6a902d8fe94616025312c9
zhangzw <zhangzw@boqii.com>
2020-07-17 11:39:22 +0800

    2020-07-17 11:39:22

--- go圣经/05/5.8/5.8title1.go
+++ go圣经/05/5.8/5.8title1.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"golang.org/x/net/html"
 	"net/http"
 	"strings"
 )
@@ -16,7 +16,30 @@ func title(url string) error {
 	ct := resp.Header.Get("Content-Type")
 	if ct != "text/html" && !strings.HasPrefix(ct,"text/html;") {
 		resp.Body.Close()
-		//return fmt.Errorf("%s has type %s, not text/html",url, ct)
-		return log.Fatalf("%s has type %s, not text/html",url, ct)
+		return fmt.Errorf("%s has type %s, not text/html",url, ct)
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" && n.FirstChild != nil {
+			fmt.Println(n.FirstChild.Data)
+		}
+	}
+	forEachNode(doc,visitNode, nil)
+	return nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
 	}
 }
