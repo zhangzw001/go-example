c59b64642e6ce9b88905ebbdd3bbe309a7bfc690
zhangzw <zhangzw@boqii.com>
2020-08-06 09:46:54 +0800

    2020-08-06 09:46:54

--- go圣经/07/7.4/tempconv.go
+++ go圣经/07/7.4/tempconv.go
@@ -13,7 +13,9 @@ type celsiusFlag struct {
 func (f *celsiusFlag) Set(s string ) error {
 	var unit string
 	var value float64
+	//调用fmt.Sscanf函数从输入s中解析一个浮点数（value）和一个字符串（unit）。虽然通常必须检查Sscanf的错误返回，但是在这个例子中我们不需要因为如果有错误发生，就没有switch case会匹配到。
 	fmt.Sscanf(s,"%f%s",&value,&unit)
+	fmt.Println(len("%f%s"))
 	fmt.Println(value,unit)
 	switch unit {
 	case "C","°C":
