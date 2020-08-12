6e2cf0d18a8dbe1c63481d3b12deb20fc7c9aa64
zhangzw <zhangzw@boqii.com>
2020-08-12 11:39:34 +0800

    2020-08-12 11:39:34

--- go圣经/07/7.10/main.go
+++ go圣经/07/7.10/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"io"
-	"os"
+	"fmt"
 )
 type ByteCounter struct {
 
 }
 func main() {
-	var w io.Writer
-	w = os.Stdout
+	//var w io.Writer
+	//w = os.Stdout
 
 	//f := w.(*os.File)
 	//c := w.(*byte.Buffer)
 
-	rw := w.(io.ReadWriter)
-	w = new(ByteCounter)
+	//rw := w.(io.ReadWriter)
+	//w = new(ByteCounter)
+
+
+	s := "aa"
+	//if v, ok := s.(string); ok {	//non-interface type string on left
+	//	fmt.Println(v)
+	//}
+	if v, ok := interface{}(s).(string) ; ok {
+		fmt.Println(v)
+	}
 }
--- go圣经/07/7.11/patherror.go
+++ go圣经/07/7.11/patherror.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
+	"syscall"
 )
 
-//// PathError records an error and the operation and file path that caused it.
-//type PathError struct {
-//	Op   string
-//	Path string
-//	Err  error
-//}
-//
-//func (e *PathError) Error() string {
-//	return e.Op + " " + e.Path + ": " + e.Err.Error()
-//}
+// PathError records an error and the operation and file path that caused it.
+type PathError struct {
+	Op   string
+	Path string
+	Err  error
+}
 
-var ErrNotExist
+func (e *PathError) Error() string {
+	return e.Op + " " + e.Path + ": " + e.Err.Error()
+}
+
+var ErrNotExist = errors.New("file does not exist")
+func IsNotExist(err error ) bool {
+	if pe, ok := err.(*PathError ); ok  {
+		err = pe.Err
+	}
+	return err == syscall.ENOENT || err == ErrNotExist
+}
 func main() {
 	_, err := os.Open("/no/such/file")
 	fmt.Println(err)
 	fmt.Printf("%#v\n",err)
+
+	fmt.Println(IsNotExist(err))
+	fmt.Println(os.IsNotExist(err))
 }
