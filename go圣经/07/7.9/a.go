6c69039db4fe3590b7ef5a2704bf9359ead3ae31
zhangzw <zhangzw@boqii.com>
2020-08-12 09:39:19 +0800

    2020-08-12 09:39:19

---
+++ go圣经/07/7.9/a.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+
+func main(){
+	k:=40
+	fmt.Println(k)
+	fmt.Println(say("hello,world","lf"))
+	fmt.Println(reflect.TypeOf(k))  //检查变量类型
+
+	fmt.Println(&k)  // 获取变量在计算机内存中的地址，可在变量名前面加上&字符。
+	// &k 引用的是变量k的值，值所在的内存地址
+	showMemoryAddress(&k)  //返回的地址是相同的
+}
+
+func showMemoryAddress(x *int){     // *int参数类型位指向整数的指针，而不是整数
+	fmt.Println(*x)  //本身就是指针，打印地址不需要  &这个符号，如果想使用指针指向的变量的值，而不是其内存地址，可在指针变量前面加上星号
+	return
+}
+
+func say(param,tt string) string {
+	return param + "--" + tt
+
+}
