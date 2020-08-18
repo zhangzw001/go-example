cc84328af13ad61b715f9ddfb27f2b18e64c00eb
zhangzw <zhangzw@boqii.com>
2020-08-18 16:39:18 +0800

    2020-08-18 16:39:18

--- go圣经/08/8.4-channel/channel4.go
+++ go圣经/08/8.4-channel/channel4.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	//ch := make(chan int , 3)
@@ -11,30 +13,34 @@ func main() {
 	//fmt.Println(<-ch)
 	//fmt.Println(<-ch)
 	//fmt.Println(cap(ch))
-
-	fmt.Println(mirroredQuery())
+	res := make(chan string ,3)
+	go mirroredQuery(res)
+	readChannel(res)
+	//fmt.Println(mirroredQuery())
 }
 
 
-func mirroredQuery() []string {
+func mirroredQuery(responses chan string ) {
 	//如果我们使用了无缓存的channel，那么两个慢的goroutines将会因为没有人接收而被永远卡住。这种情况，称为goroutines泄漏，这将是一个BUG。和垃圾变量不同，泄漏的goroutines并不会被自动回收，因此确保每个不再需要的goroutine能正常退出是重要的。
-	responses := make(chan string , 3 )
-	var l []string
-	go func() { responses <- request("asia.gopl.io")}()
-	go func() { responses <- request("eurepo.gopl.io")}()
-	go func() { responses <- request("americas.gopl.io")}()
-	// 方法1
-	//for x := 0 ; x < 3 ; x ++ {
-	//	l = append(l, <-responses)
-	//}
-	// 方法2
-	for x := range responses {
-		l = append(l, x)
-	}
-	close(responses)
-	return l
+	//responses := make(chan string, 3)
+	 responses <- request("asia.gopl.io")
+	 responses <- request("eurepo.gopl.io")
+	 responses <- request("americas.gopl.io")
+	 //responses <- request("americas2.gopl.io")
+	 defer close(responses)
 }
 
+func readChannel(ch chan string ) {
+	//////////
+	for x := 0 ; x < 3 ; x ++ {
+		fmt.Println(<-ch)
+	}
+	//////////
+	//for x := range ch {
+	//	//l = append(l, x)
+	//	fmt.Println(x)
+	//}
+}
 func request(hostname string ) ( response string ) {
 	return hostname
 }
