079a18ef71b6e7c9b8592a30923c15c1c7c0b946
zhangzw <zhangzw@boqii.com>
2020-08-21 13:39:22 +0800

    2020-08-21 13:39:22

--- go圣经/08/8.7/work/work8.8.go
+++ go圣经/08/8.7/work/work8.8.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -21,19 +22,26 @@ func echo(c net.Conn, shout string, delay time.Duration) {
 
 func handleConn(c net.Conn) {
 	ch := make(chan string )
+	input := bufio.NewScanner(c)
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
 	go func() {
-		input := bufio.NewScanner(c)
+		wg.Done()
+		for {
+			select {
+			case <-time.After(3 * time.Second):
+				c.Close()
+			case input := <-ch:
+				echo(c, input, 1*time.Second)
+			}
+		}
+	}()
 		for input.Scan() {
 			ch <- input.Text()
 		}
-	}()
+	wg.Wait()
 
-	select {
-	case <-time.After(3 * time.Second):
-		c.Close()
-	case input := <-ch:
-		echo(c,input, 1*time.Second)
-	}
 }
 
 func main() {
@@ -45,6 +53,50 @@ func main() {
 
 	for {
 		conn, _ := listener.Accept()
+		//go handleConn(conn)
 		go handleConn(conn)
 	}
+}
+
+
+
+//网上别人写的
+func handleConn2(c net.Conn) {
+	input := bufio.NewScanner(c)
+	var wg sync.WaitGroup
+	var message = make(chan string)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			//case <-time.After(6 * time.Second):
+			case <-time.After(6 * time.Second):
+				c.Close()
+			case mes := <-message:
+				wg.Add(1)
+				echo(c,mes,1*time.Second)
+				//go func(c net.Conn, shout string, delay time.Duration) {
+				//	defer wg.Done()
+				//	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+				//	time.Sleep(delay)
+				//	fmt.Fprintln(c, "\t", shout)
+				//	time.Sleep(delay)
+				//	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+				//	//ch<-struct{}{}
+				//}(c, mes, 1*time.Second)
+
+			}
+		}
+	}()
+	for input.Scan() {
+		text := input.Text()
+		message <- text
+	}
+
+	wg.Wait()
+	//cw := c.(*net.TCPConn)
+	//cw.CloseWrite()
+
+	c.Close()
 }
