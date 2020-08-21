aac63404e38d500f0611191cef06ad3bda6c9bd0
zhangzw <zhangzw@boqii.com>
2020-08-21 16:39:37 +0800

    2020-08-21 16:39:37

--- go圣经/08/8.8/du1.go
+++ go圣经/08/8.8/du1.go
@@ -7,7 +7,14 @@ import (
 	"os"
 	"path/filepath"
 )
+const (
+	_ = 1 <<(10 * iota)
+	K
+	M
+	G
+	T
 
+)
 func main() {
 	flag.Parse()
 	roots := flag.Args()
@@ -16,7 +23,32 @@ func main() {
 	}
 	fileSizes := make(chan int64)
 	go func() {
-		
+		for _, root := range roots {
+			walkDir(root, fileSizes)
+		}
+		close(fileSizes)
+	}()
+
+	var nfiles, nbytes int64
+	for size := range fileSizes {
+		nfiles++
+		nbytes += size
+	}
+
+	printDiskUsage(nfiles,nbytes)
+}
+
+func printDiskUsage(nfiles,nbytes int64) {
+	switch  {
+	case nbytes > G:
+		fmt.Printf("%d files %.4fGB\n",nfiles,float64(nbytes)/float64(G))
+	case nbytes > M:
+		fmt.Printf("%d files %.4fMB\n",nfiles,float64(nbytes)/float64(M))
+	case nbytes > K:
+		fmt.Printf("%d files %.4fMB\n",nfiles,float64(nbytes/K))
+	default:
+		fmt.Printf("%d files %.4fB\n",nfiles,float64(nbytes))
+
 	}
 }
 
@@ -35,7 +67,7 @@ func walkDir(dir string, fileSizes chan<-int64) {
 func dirents(dir string) []os.FileInfo {
 	entries,err := ioutil.ReadDir(dir)
 	if err != nil {
-		fmt.Fprintf(os.Stderr,"du1: ^v\n",err)
+		fmt.Fprintf(os.Stderr,"du1: %v\n",err)
 		return nil
 	}
 	return entries
