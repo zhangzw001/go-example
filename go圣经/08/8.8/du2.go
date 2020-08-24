024c31b0b0275d0c4d1e508b88f934d11501fb52
zhangzw <zhangzw@boqii.com>
2020-08-24 15:41:51 +0800

    2020-08-24 15:41:51

---
+++ go圣经/08/8.8/du2.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+const (
+	_ = 1 <<(10 * iota)
+	K
+	M
+	G
+	T
+
+)
+func main() {
+	flag.Parse()
+	roots := flag.Args()
+
+	var verbose = flag.Bool("v",false,"show verbose progress messages")
+
+}
+
+func printDiskUsage(nfiles,nbytes int64) {
+	switch  {
+	case nbytes > G:
+		fmt.Printf("%d files %.4fGB\n",nfiles,float64(nbytes)/float64(G))
+	case nbytes > M:
+		fmt.Printf("%d files %.4fMB\n",nfiles,float64(nbytes)/float64(M))
+	case nbytes > K:
+		fmt.Printf("%d files %.4fMB\n",nfiles,float64(nbytes/K))
+	default:
+		fmt.Printf("%d files %.4fB\n",nfiles,float64(nbytes))
+
+	}
+}
+
+func walkDir(dir string, fileSizes chan<-int64) {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subdir := filepath.Join(dir, entry.Name())
+			fmt.Println(subdir,dir,entry.Name())
+			walkDir(subdir,fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+
+func dirents(dir string) []os.FileInfo {
+	entries,err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr,"du1: %v\n",err)
+		return nil
+	}
+	return entries
+}
