2932f89b9f90b432f37e0f04787e19ef2db2c35f
zhangzw <zhangzw@boqii.com>
2020-08-24 16:41:53 +0800

    2020-08-24 16:41:53

--- go圣经/08/8.8/du2.go
+++ go圣经/08/8.8/du2.go
@@ -6,54 +6,91 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"time"
 )
+
 const (
-	_ = 1 <<(10 * iota)
+	_ = 1 << (10 * iota)
 	K
 	M
 	G
 	T
-
 )
+
+var verbose = flag.Bool("v", false, "show verbose progress messages")
+
 func main() {
 	flag.Parse()
 	roots := flag.Args()
 
-	var verbose = flag.Bool("v",false,"show verbose progress messages")
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+	fileSizes := make(chan int64)
+	go func() {
+		for _, root := range roots {
+			walkDir(root, fileSizes)
+		}
+		close(fileSizes)
+	}()
 
+	var tick <-chan time.Time
+	var n chan 
+	if *verbose {
+		tick = time.Tick(100 * time.Millisecond)
+	} else {
+		n <- struct{}{}
+	}
+	var nfiles, nbytes int64
+
+loop:
+	for {
+		select {
+		case size, ok := <-fileSizes:
+			if !ok {
+				break loop
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		case <-n:
+			printDiskUsage(nfiles, nbytes)
+			close(n)
+		}
+	}
 }
 
-func printDiskUsage(nfiles,nbytes int64) {
-	switch  {
+func printDiskUsage(nfiles, nbytes int64) {
+	switch {
 	case nbytes > G:
-		fmt.Printf("%d files %.4fGB\n",nfiles,float64(nbytes)/float64(G))
+		fmt.Printf("%d files %.4fGB\n", nfiles, float64(nbytes)/float64(G))
 	case nbytes > M:
-		fmt.Printf("%d files %.4fMB\n",nfiles,float64(nbytes)/float64(M))
+		fmt.Printf("%d files %.4fMB\n", nfiles, float64(nbytes)/float64(M))
 	case nbytes > K:
-		fmt.Printf("%d files %.4fMB\n",nfiles,float64(nbytes/K))
+		fmt.Printf("%d files %.4fMB\n", nfiles, float64(nbytes/K))
 	default:
-		fmt.Printf("%d files %.4fB\n",nfiles,float64(nbytes))
+		fmt.Printf("%d files %.4fB\n", nfiles, float64(nbytes))
 
 	}
 }
 
-func walkDir(dir string, fileSizes chan<-int64) {
+func walkDir(dir string, fileSizes chan<- int64) {
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
 			subdir := filepath.Join(dir, entry.Name())
-			fmt.Println(subdir,dir,entry.Name())
-			walkDir(subdir,fileSizes)
+			//fmt.Println(subdir, dir, entry.Name())
+			walkDir(subdir, fileSizes)
 		} else {
 			fileSizes <- entry.Size()
 		}
 	}
 }
 
-
 func dirents(dir string) []os.FileInfo {
-	entries,err := ioutil.ReadDir(dir)
+	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
-		fmt.Fprintf(os.Stderr,"du1: %v\n",err)
+		fmt.Fprintf(os.Stderr, "du1: %v\n", err)
 		return nil
 	}
 	return entries
