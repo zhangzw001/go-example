ff56bf22635ab0f206277b9fb262a17d0d4f5862
zhangzw <zhangzw@boqii.com>
2020-09-03 16:41:52 +0800

    2020-09-03 16:41:52

---
+++ go圣经/09/9.7/memo/memo5/memo5.go
@@ -0,0 +1,70 @@
+package memo5
+
+
+type request struct {
+	key 		string
+	response 	chan <- result
+}
+
+
+
+type Memo struct {
+	requests 	chan request
+}
+
+type result struct {
+	value 	interface{}
+	err 	error
+}
+
+type entry struct {
+	res 	result
+	ready 	chan struct{}
+}
+
+type Func func(key string ) ( interface{}, error )
+
+
+
+func New(f Func) *Memo {
+	memo := &Memo{requests: make(chan request)}
+	go memo.server(f)
+	return memo
+}
+
+/*
+这些修改使性能再次得到了提升，但有一些URL被获取了两次。这种情况在两个以上的goroutine同一时刻调用Get来请求同样的URL时会发生。多个goroutine一起查询cache，发现没有值，然后一起调用f这个慢不拉叽的函数。在得到结果后，也都会去更新map。其中一个获得的结果会覆盖掉另一个的结果。
+
+*/
+func ( memo *Memo) Get(key string ) (value interface{}, err error ) {
+	response := make(chan result)
+	memo.requests <- request{key,response}
+	res := <-response
+	return res.value, res.err
+}
+
+func ( memo *Memo) Close() { close(memo.requests) }
+
+func ( memo *Memo) server(f Func) {
+	cache := make(map[string]*entry)
+	for req := range memo.requests {
+		e := cache[req.key]
+		if e == nil {
+			e = &entry{ready: make(chan struct{})}
+			cache[req.key] = e
+			go e.call(f, req.key)
+		}
+
+		go e.deliver(req.response)
+	}
+}
+
+func (e *entry) call(f Func, key string ) {
+	e.res.value, e.res.err = f(key)
+	close(e.ready)
+}
+
+func (e *entry) deliver(response chan<-result) {
+	<-e.ready
+	response <- e.res
+}
