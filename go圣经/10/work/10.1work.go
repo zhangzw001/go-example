14ace32ef3c084f5e2d3b307405e987a5d4b00b0
zhangzw <zhangzw@boqii.com>
2020-09-07 14:41:49 +0800

    2020-09-07 14:41:49

--- go圣经/10/work/10.1work.go
+++ go圣经/10/work/10.1work.go
@@ -1,14 +1,17 @@
-package main
+package work
 
 import (
+	"flag"
 	"fmt"
 	"image"
 	"image/jpeg"
-	_ "image/png"
 	"io"
 	"log"
 	"os"
 )
+
+//练习 10.1： 扩展jpeg程序，以支持任意图像格式之间的相互转换，使用image.Decode检测支持的格式类型，然后通过flag命令行标志参数选择输出的格式。
+
 
 func main() {
 	f ,err := os.Open("/tmp/test1.png")
@@ -16,7 +19,10 @@ func main() {
 		log.Fatal(err)
 	}
 	defer f.Close()
-	if err := toJPEG(f,os.Stdout); err != nil {
+
+	var type string
+	type : = flag.String(type,"jpeg","输入图片格式")
+	if err := toImg(f,os.Stdout); err != nil {
 		fmt.Fprintf(os.Stderr,"jpeg: %v\n", err)
 		os.Exit(1)
 	}
@@ -25,10 +31,11 @@ func main() {
 }
 
 
-func toJPEG(in io.Reader , out io.Writer) error {
+func toImg(in io.Reader , out io.Writer) error {
 	img, kind , err := image.Decode(in)
 	if err != nil { return err}
 	fmt.Fprintf(os.Stderr, "Input format=%v\n",kind)
+
 	return jpeg.Encode(out,img,&jpeg.Options{Quality:95})
 
 }
