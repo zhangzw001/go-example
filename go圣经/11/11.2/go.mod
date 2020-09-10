8698904495634c0ade83c3fb67c9461b6f547eae
zhangzw <zhangzw@boqii.com>
2020-09-10 12:41:55 +0800

    2020-09-10 12:41:55

--- go圣经/11/11.2/go.mod
+++ go圣经/11/11.2/go.mod
@@ -8,5 +8,5 @@ replace storageMail => ./storageMail
 
 require (
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
--- go圣经/11/11.2/storageMail/gomail.go
+++ go圣经/11/11.2/storageMail/gomail.go
@@ -7,33 +7,39 @@ import (
 )
 
 type mail struct {
-	serverHost string
-	serverPort int
-	fromUser   string
-	fromPass   string
+	serverHost 	string
+	serverPort 	int
+	fromUser   	string
+	fromPass   	string
+	msg 		*gomail.Message
 }
 
 func (m *mail )init() {
 	fmt.Println("init")
 	*m = mail{
-		serverHost: "smtp.boqii.com",
-		serverPort: 465,
-		fromUser: "bq_zabbix",
-		fromPass: "boqii@3124",
+		serverHost: "",
+		serverPort: 1,
+		fromUser: "",
+		fromPass: "",
+		msg: gomail.NewMessage(),
 	}
+	m.msg.SetHeader("From","bq_zabbix@boqii.com")
+	m.msg.SetHeader("To","zhangzw@boqii.com")
+	m.msg.SetAddressHeader("Cc","zhangzw@boqii.com","zhangzw")
+	m.msg.SetBody("text/html","hello <b>zhangzw</b>  <a href=\"www.baidu.com\">www.baidu.com</a>")
+	m.msg.SetHeader("Subject", "Hello!")
+
 }
 
 func GomailLearn1() {
 	var m1 mail
 	m1.init()
-	fmt.Println(m1)
+
 	d := gomail.NewDialer(m1.serverHost,m1.serverPort,m1.fromUser,m1.fromPass)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 
-	m := gomail.NewMessage()
-	m.SetHeader("From","bq_zabbix@boqii.com")
-	m.SetHeader("To","zhangzw@boqii.com")
-	m.SetAddressHeader("Cc","zhangzw@boqii.com","zhangzw")
-	m.SetBody("text/html","hello <b>zhangzw</b>  <a href=\"www.baidu.com\">www.baidu.com</a>")
+	if err := d.DialAndSend(m1.msg); err != nil {
+		panic(err)
+	}
 
 }
