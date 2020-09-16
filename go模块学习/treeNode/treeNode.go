c2d1c38cf5f720febc5c992f11e0f005e488f44c
zhangzw <zhangzw@boqii.com>
2020-09-16 11:41:53 +0800

    2020-09-16 11:41:53

--- go模块学习/treeNode/treeNode.go
+++ go模块学习/treeNode/treeNode.go
@@ -16,26 +16,37 @@ type Tree interface {
 	Print()
 }
 //创建二叉树
-func (tree *treeNode) Add(val int ){
-	tree.value = val
+func CreateTree(val int) *treeNode {
+	return &treeNode{val,nil,nil}
 }
+
 
 // 打印树
 func (tree *treeNode) Print() {
 	if tree != nil {
 		fmt.Printf("%d ",tree.value)
+		if tree.left != nil && tree.right != nil {
+			tree.left.Print()
+			tree.right.Print()
+		}
 	}
 }
 
+
 func main() {
-	t := &treeNode{}
+	var t1 *treeNode
+	t1 = CreateTree(1)
+	t1.left = CreateTree(2)
+	t1.right = CreateTree(3)
+	t1.Print()
 
 
-	t.Add(10)
-	t.left.Add(11)
-	t.right.Add(12)
 
-	t.Print()
+
+	//(t.left).Add(11)
+	//(t.right).Add(12)
+
+	//t.Print()
 	//t.left.Print()
 
 }
