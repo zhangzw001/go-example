e866f74eceb9ce62dbdcc631e7c5275af97fbda0
zhangzw <zhangzw@boqii.com>
2020-09-17 14:41:58 +0800

    2020-09-17 14:41:58

---
+++ go圣经/02/2.6/popcount/2.6.2popcount_test.go
@@ -0,0 +1,44 @@
+package popcount
+
+import (
+	"reflect"
+	"testing"
+)
+
+func assert(t *testing.T,expected,actual interface{}){
+	if !reflect.DeepEqual(expected,actual){
+		t.Errorf("(expected,actual) = (%v,%v)\n",expected,actual)
+	}
+}
+
+func TestPopCount(t *testing.T) {
+	assert(t,32, PopCount(0x1234567890ABCDEF))
+}
+
+
+func TestPopCountByLoop(t *testing.T) {
+	assert(t, 32, PopCountByLoop(0x1234567890ABCDEF))
+}
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountByLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountByLoop(0x1234567890ABCDEF)
+	}
+}
+func BenchmarkPopCountByLoopShift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountByLoopShift(0x1234567890ABCDEF)
+	}
+}
+func BenchmarkPopCountByAnd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountByAnd(0x1234567890ABCDEF)
+	}
+}
+
