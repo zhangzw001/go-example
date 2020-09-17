8d511ccea1deebf139bf8fb4686f782f7ac19ea1
zhangzw <zhangzw@boqii.com>
2020-09-17 16:41:53 +0800

    2020-09-17 16:41:53

---
+++ go圣经/12/12.2/format/format_test.go
@@ -0,0 +1,27 @@
+package format
+
+import (
+	"fmt"
+	"gopl.io/ch12/format"
+	"testing"
+	"time"
+)
+
+func ExampleAny() {
+	var x int64 = 1
+	var d time.Duration = 1 * time.Nanosecond
+	fmt.Println(format.Any(x))                  // "1"
+	fmt.Println(format.Any(d))                  // "1"
+	fmt.Println(format.Any([]int64{x}))         // "[]int64 0x8202b87b0"
+	fmt.Println(format.Any([]time.Duration{d})) // "[]time.Duration 0x8202b87e0"
+}
+
+
+func TestAny(t *testing.T) {
+	var x int64 = 1
+	var d time.Duration = 1 * time.Nanosecond
+	fmt.Println(format.Any(x))                  // "1"
+	fmt.Println(format.Any(d))                  // "1"
+	fmt.Println(format.Any([]int64{x}))         // "[]int64 0x8202b87b0"
+	fmt.Println(format.Any([]time.Duration{d})) // "[]time.Duration 0x8202b87e0"
+}
