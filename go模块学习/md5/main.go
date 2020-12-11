1918aa1c622bc2b9e9faf9ffe875dbdb30a8159a
zhangzw <zhangzw@boqii.com>
2020-12-11 17:42:31 +0800

    2020-12-11 17:42:31

---
+++ go模块学习/md5/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"time"
+)
+
+func main() {
+	key , _ := MD5("abcd"+time.Now().String())
+	fmt.Println(key)
+	key , _ = MD5("abcd")
+	fmt.Println(key)
+}
+
+
+func MD5(s string) (string ,error) {
+	secret := md5.New()
+	_,err := secret.Write([]byte(s))
+	if err != nil {
+		return "", err
+	}
+	//secret.Sum([]byte(s))
+	return hex.EncodeToString(secret.Sum(nil)),nil
+}
